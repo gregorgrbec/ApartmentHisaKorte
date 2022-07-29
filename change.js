ca9b367a56b941fc4de8f99c2464253d2a6a59ec
Gregor <ggrbec@gmail.com>
2022-07-29 17:31:37 +0200

    Corrected contact links, improved shadow, added footer

--- change.js
+++ change.js
@@ -4,11 +4,19 @@ function change(value) {
   var text = "";
 
   if (value == "phone") {
-    text = "051818866";
-    phone.innerHTML = text;
+    text = "+38656402255";
+    // phone.innerHTML = text;
+    p1 = document.createElement("p");
+    p1.innerHTML = text;
+    p1.classList.add("p1");
+    phone.replaceWith(p1);
   }
   if (value == "email") {
-    text = "ggrbec@gmail.com";
-    email.innerHTML = text;
+    text = "info@delta-sp.si";
+    // email.innerHTML = text;
+    p2 = document.createElement("p");
+    p2.innerHTML = text;
+    p2.classList.add("p2");
+    email.replaceWith(p2);
   }
 }
